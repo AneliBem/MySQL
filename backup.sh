0a492b437818dc0e5b7fc14d52e95369c3600672
AneliBem <tolya.bem@gmail.com>
2025-02-11 13:36:48 +0200

    AUTO backup for SQL

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+MYSQLDUMP="/opt/lampp/bin/mysqldump"
+
+$MYSQLDUMP -u root tabl > ~/backup_$DATE.sql
+
+cd ~/sqlbase
+
+mv ~/backup_$DATE.sql backup.sql
+
+git add backup.sql
+git commit -m "Auto backup $DATE"
+git push origin main
