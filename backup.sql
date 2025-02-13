84becb2c989bdce0262a17e851b6377cfe537c56
AneliBem <tolya.bem@gmail.com>
2025-02-14 01:00:41 +0200

    Auto backup 2025-02-14_01-00-40

--- backup.sql
+++ backup.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `olderNumber` int(11) DEFAULT NULL,
+  `orderNumber` int(11) DEFAULT NULL,
   `shopId` int(11) DEFAULT NULL,
   `personId` int(11) DEFAULT NULL,
   `date_time` datetime DEFAULT current_timestamp(),
@@ -57,7 +57,7 @@ CREATE TABLE `shop` (
   `title` varchar(100) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,6 +66,7 @@ CREATE TABLE `shop` (
 
 LOCK TABLES `shop` WRITE;
 /*!40000 ALTER TABLE `shop` DISABLE KEYS */;
+INSERT INTO `shop` VALUES (1,'iPhone',3000),(2,'iPad',4000),(3,'iMac',6000),(4,'TV',300),(5,'Router',250),(6,'Swich',300);
 /*!40000 ALTER TABLE `shop` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-13 19:49:44
+-- Dump completed on 2025-02-14  1:00:41
